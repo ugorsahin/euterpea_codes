c267445afa61f89801d1b895736a33dac82cf8a0
Uğur Şahin <e2036564@ceng.metu.edu.tr>
2019-06-22 19:40:07 +0300

    Arrangement on files

--- book9.hs
+++ book9.hs
@@ -44,19 +44,26 @@ ss pat n tr te = transpose tr $ tempo te $ simToMusic $fringe n $ selfSim pat
 
 m0,m1 :: [SNote]
 m0 = [(1,2),(1,0),(1,5),(1,7)]
+
 tm0 = instrument Vibraphone (ss m0 3 50 20)
+
 ttm0 = tm0 :=: transpose (-12) tm0
+
 m1 = [(1,0),(0.5,0),(0.5,0)]
+
 tm1 = instrument Percussion (ss m1 2 43 2)
+
 m2 :: [SNote]
 m2 = [(dqn,0),(qn,4)]
 tm2 = ss m2 4 50 (1/50)
+
 m3 :: [SNote]
 m3 = [(hn,3),(qn,4),(qn,0),(hn,6)]
 tm3 = ss m3 4 50 (1/4)
 ttm3 = let l1 = instrument Flute tm3
            l2 = instrument AcousticBass $ transpose (-9) (revM tm3)
            in l1 :=: l2
+
 m4 :: [SNote]
 m4 = [(hn,3),(hn,8),(hn,22),(qn,4),(qn,7),(qn,21),(qn,0),(qn,5),(qn,15),(wn,6),(wn,9),(wn,19)]
 tm4 = ss m4 3 50 8
---
+++ sesler/signal.hs
@@ -0,0 +1,102 @@
+
+{-# LANGUAGE Arrows #-}
+import Euterpea
+import Control.Arrow((>>>),(<<<),arr)
+
+
+constA :: Clock c => Double -> SigFun c () Double
+constA ph = proc () -> do
+	s <- oscFixed ph -< ()
+	outA -< s
+
+s1 :: Clock c => SigFun c () Double
+s1 = proc () -> do
+    s <- oscFixed 440 -< ()
+    outA -< s
+
+tab1 :: Table
+tab1 = tableSinesN 4096 [1]
+
+tab2 :: Table
+tab2 = tableSinesN 4096 [1,0.5,0.33]
+
+s2 :: Clock c => SigFun c () Double
+s2 = proc () -> do
+    osc tab1 0 -< 440
+
+s3 :: Clock c => SigFun c () Double
+s3 = proc () -> do
+    osc tab2 0 -< 440
+
+s4 :: Clock c => SigFun c () Double
+s4 = proc () -> do
+    f0 <- oscFixed 440 -< ()
+    f1 <- oscFixed 880 -< ()
+    f2 <- oscFixed 1320 -< ()
+    outA -< (f0 + 0.5*f1 + 0.33*f2) / 1.83
+
+vibrato :: Clock c => Double -> Double -> SigFun c Double Double
+vibrato vfrq dep = proc afrq -> do
+    vib <- osc tab1 0 -< vfrq
+    aud <- osc tab2 0 -< afrq + vib * dep
+    outA -< aud
+
+simpleClip :: Clock c => SigFun c Double Double
+simpleClip = arr f where
+	f x = if abs x <= 1.0 then x else signum x
+
+
+s5 :: AudSF () Double
+s5 = constA 1000 >>> vibrato 5 1000 >>> simpleClip
+
+simpleInstr1 :: InstrumentName
+simpleInstr1 = CustomInstrument "Simple Instrument"
+
+myInstr :: Instr (AudSF () Double)
+myInstr dur ap vol [vfrq,dep] =
+    proc () -> do
+        vib <- osc tab1 0 -< vfrq
+        aud <- osc tab2 0 -< apToHz ap + vib * dep
+        outA -< aud
+
+type InstrMap1 a = [(InstrumentName, Instr a)]
+-- makeitdb :: Music --D A F U Q
+-- ???????
+mel :: Music1
+mel = 
+    let m = Euterpea.line [na1 (c 4 en), na1 (ef 4 en), na1 (f 4 en),
+         na2 (af 4 qn), na1 (f 4 en), na1 (af 4 en),
+         na2 (bf 4 qn), na1 (af 4 en), na1 (bf 4 en),
+         na1 (c 5 en), na1 (ef 5 en), na1 (f 5 en),
+         na3 (af 5 wn)] where
+             na1 (Prim (Note dr p)) = Prim (Note dr (p, [Params [0,0]]))
+             na2 (Prim (Note dr p)) = Prim (Note dr (p, [Params [5,10]]))
+             na3 (Prim (Note dr p)) = Prim (Note dr (p, [Params [5,20]]))
+    in instrument simpleInstr1 m
+
+prefixes :: [a] -> [[a]]
+prefixes [] = []
+prefixes (x:xs) = let f pf = x:pf
+                  in [x]:map f (prefixes xs)
+
+prefix :: [Music a] -> Music a
+prefix mel = let m1 = line(concat(prefixes mel))
+                 m2 = transpose 12 (line(concat(prefixes(reverse mel))))
+                 m = instrument Flute m2
+              in m :+: transpose 5 m :+: m
+
+rep :: (Music a -> Music a) -> (Music a -> Music a) -> Int -> Music a -> Music a
+rep f g 0 m = rest 0
+rep f g n m = m :=: g (rep f g (n-1) (f m))
+
+myInstrMap :: InstrMap1 (AudSF () Double)
+myInstrMap = [(simpleInstr1, myInstr)]
+
+mel1 = prefix [c 5 qn, f 4 qn, g 4 qn,  b 4 qn]
+
+(dr,sf) = renderSF mel1 myInstrMap
+main = outFile "simple.wav" dr sf
+
+--why? 
+
+    
